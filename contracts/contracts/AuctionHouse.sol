df395f636ec49d778999fe1f682e2e356d15b3e1
zion <yzyrmb8@outlook.com>
2023-11-05 20:29:33 -0500

    AuctionHouse.sol

---
+++ contracts/contracts/AuctionHouse.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AuctionHouse is ERC20, {
+    constructor(address initialOwner)
+        ERC20("AuctionHouse", "AUC")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
