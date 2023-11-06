cb194f5d15950704075ac80da508463d6fe8400f
zion <yzyrmb8@outlook.com>
2023-11-05 23:58:14 -0500

    Created AUC and AUT

--- contracts/contracts/AuctionHouse.sol
+++ contracts/contracts/AuctionHouse.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./AuctionHouseCoin.sol";
+import "./AuctionDigitalItem.sol";
 
-contract AuctionHouse is ERC20, {
-    constructor(address initialOwner)
-        ERC20("AuctionHouse", "AUC")
-    {}
-
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
-    }
+contract AuctionHouse {
+    
 }
