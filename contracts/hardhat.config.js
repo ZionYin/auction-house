9652d9d412bea9d37f6a3981b934f4dbc9984def
zion <yzyrmb8@outlook.com>
2023-11-09 01:34:15 -0500

    Add etherscan and sourcify configurations, update deploy script to use contract factory.

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.20",
@@ -15,4 +16,10 @@ module.exports = {
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
 };
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,11 +1,27 @@
+// const { ethers, network, run } = require("hardhat");
+
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
-  const token = await ethers.deployContract("AuctionToken");
-  console.log("Token contract address:", await token.getAddress());
+  const tokenFactory = await ethers.getContractFactory("AuctionToken");
+  const token = await tokenFactory.deploy();
+  await token.waitForDeployment();
+  console.log("Token contract address:", token.target);
 
-  const auction = await ethers.deployContract("AuctionHouse", [token.getAddress(), deployer.address]);
-  console.log("Auction House contract address:", await auction.getAddress());
+  const auctionFactory = await ethers.getContractFactory("AuctionHouse");
+  const auction = await auctionFactory.deploy(token.target, deployer.address);
+  await auction.waitForDeployment();
+  console.log("Auction House contract address:", auction.target);
+
+  console.log("Verifying contracts on etherscan...");
+  await hre.run("verify:verify", {
+    address: token.target,
+    constructorArguments: [],
+  });
+  await hre.run("verify:verify", {
+    address: auction.target,
+    constructorArguments: [token.target, deployer.address],
+  });
 }
 
 main()
