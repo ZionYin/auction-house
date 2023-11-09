b62b2286fadd01d01cfaf73e211a2259f09e5511
zion <yzyrmb8@outlook.com>
2023-11-09 11:35:36 -0500

    Refactor deployment script and add network check.

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,27 +1,30 @@
-// const { ethers, network, run } = require("hardhat");
+const { network } = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
   const tokenFactory = await ethers.getContractFactory("AuctionToken");
   const token = await tokenFactory.deploy();
-  await token.waitForDeployment();
   console.log("Token contract address:", token.target);
 
   const auctionFactory = await ethers.getContractFactory("AuctionHouse");
   const auction = await auctionFactory.deploy(token.target, deployer.address);
-  await auction.waitForDeployment();
   console.log("Auction House contract address:", auction.target);
 
-  console.log("Verifying contracts on etherscan...");
-  await hre.run("verify:verify", {
-    address: token.target,
-    constructorArguments: [],
-  });
-  await hre.run("verify:verify", {
-    address: auction.target,
-    constructorArguments: [token.target, deployer.address],
-  });
+  if (network.name === "sepolia") {
+    console.log("Waiting for block confirmations...");
+    await auction.deploymentTransaction().wait(3);
+
+    console.log("Verifying contracts on etherscan...");
+    await hre.run("verify:verify", {
+      address: token.target,
+      constructorArguments: [],
+    });
+    await hre.run("verify:verify", {
+      address: auction.target,
+      constructorArguments: [token.target, deployer.address],
+    });
+  }
 }
 
 main()
